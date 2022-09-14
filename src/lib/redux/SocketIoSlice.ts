6b6ded1d0ff214222edb294671a3c2fbdd0131ba
Dave Lichterman <me@lavid.me>
2022-09-13 17:26:33 -0700

    v 0.0.15
    * package ups
    * add more selector logic to SocketIoSlice for WCPShared changes

--- src/lib/redux/SocketIoSlice.ts
+++ src/lib/redux/SocketIoSlice.ts
@@ -1,6 +1,6 @@
 import { createEntityAdapter, createSlice, EntityState, PayloadAction } from "@reduxjs/toolkit";
 import { parseISO } from 'date-fns';
-import type { FulfillmentConfig, ICatalog, ICategory, IMenu, IOption, IOptionType, IProduct, IProductInstance, IProductInstanceFunction, IWSettings, OrderInstanceFunction } from "@wcp/wcpshared";
+import type { CatalogCategoryEntry, CatalogModifierEntry, CatalogProductEntry, FulfillmentConfig, ICatalog, ICatalogSelectors, IMenu, IOption, IProductInstance, IProductInstanceFunction, IWSettings, OrderInstanceFunction } from "@wcp/wcpshared";
 
 export const TIMING_POLLING_INTERVAL = 30000;
 
@@ -9,15 +9,28 @@ export interface CurrentTimes {
   currentLocalTime: number;
 }
 
-export const OrderInstanceFunctionsAdapter = createEntityAdapter<OrderInstanceFunction>({ selectId: entry => entry.id });
 export const ProductInstanceFunctionsAdapter = createEntityAdapter<IProductInstanceFunction>({ selectId: entry => entry.id });
-export const IProductsAdapter = createEntityAdapter<IProduct>({ selectId: entry => entry.id });
+export const OrderInstanceFunctionsAdapter = createEntityAdapter<OrderInstanceFunction>({ selectId: entry => entry.id });
+export const IProductEntriesAdapter = createEntityAdapter<CatalogProductEntry>({ selectId: entry => entry.product.id });
 export const IProductInstancesAdapter = createEntityAdapter<IProductInstance>({ selectId: entry => entry.id });
-export const IOptionTypesAdapter = createEntityAdapter<IOptionType>({ selectId: entry => entry.id });
-export const IOptionsAdapter = createEntityAdapter<IOption>({ selectId: entry => entry.id });
-export const ICategoriesAdapter = createEntityAdapter<ICategory>({ selectId: entry => entry.id });
-export const { selectAll: getCategories, selectById: getCategoryById, selectIds: getCategoryIds } =
-  ICategoriesAdapter.getSelectors();
+export const ICategoryEntriesAdapter = createEntityAdapter<CatalogCategoryEntry>({ selectId: entry => entry.category.id });
+export const ModifierTypeEntriesAdapter = createEntityAdapter<CatalogModifierEntry>({ selectId: entry => entry.modifierType.id });
+export const ModifierOptionsAdapter = createEntityAdapter<IOption>({ selectId: entry => entry.id });
+
+export const { selectAll: getProductInstanceFunctions, selectById: getProductInstanceFunctionById, selectIds: getProductInstanceFunctionIds } =
+  ProductInstanceFunctionsAdapter.getSelectors();
+export const { selectAll: getOrderInstanceFunctions, selectById: getOrderInstanceFunctionById, selectIds: getOrderInstanceFunctionIds } =
+  OrderInstanceFunctionsAdapter.getSelectors();
+export const { selectAll: getProductEntries, selectById: getProductEntryById, selectIds: getProductEntryIds } =
+  IProductEntriesAdapter.getSelectors();
+export const { selectAll: getProductInstances, selectById: getProductInstanceById, selectIds: getProductInstanceIds } =
+  IProductInstancesAdapter.getSelectors();
+export const { selectAll: getCategoryEntries, selectById: getCategoryEntryById, selectIds: getCategoryEntryIds } =
+  ICategoryEntriesAdapter.getSelectors();
+export const { selectAll: getModifierTypeEntries, selectById: getModifierTypeEntryById, selectIds: getModifierTypeEntryIds } =
+  ModifierTypeEntriesAdapter.getSelectors();
+export const { selectAll: getModifierOptions, selectById: getModifierOptionById, selectIds: getModifierOptionIds } =
+  ModifierOptionsAdapter.getSelectors();
 
 export interface SocketIoState {
   pageLoadTime: number;
@@ -29,18 +42,17 @@ export interface SocketIoState {
 
   menu: IMenu | null;
   catalog: ICatalog | null;
-  modifiers: EntityState<IOptionType>;
+  modifierEntries: EntityState<CatalogModifierEntry>;
   modifierOptions: EntityState<IOption>;
-  products: EntityState<IProduct>;
+  products: EntityState<CatalogProductEntry>;
   productInstances: EntityState<IProductInstance>;
-  categories: EntityState<ICategory>;
+  categories: EntityState<CatalogCategoryEntry>;
   productInstanceFunctions: EntityState<IProductInstanceFunction>;
   orderInstanceFunctions: EntityState<OrderInstanceFunction>;
   fulfillments: Record<string, FulfillmentConfig> | null;
   settings: IWSettings | null;
   status: 'NONE' | 'START' | 'CONNECTED' | 'FAILED';
 }
-
 
 const initialState: SocketIoState = {
   pageLoadTime: 0,
@@ -52,11 +64,11 @@ const initialState: SocketIoState = {
   serverTime: null,
   catalog: null,
   fulfillments: null,
-  modifiers: IOptionTypesAdapter.getInitialState(),
-  modifierOptions: IOptionsAdapter.getInitialState(),
-  products: IProductsAdapter.getInitialState(),
+  modifierEntries: ModifierTypeEntriesAdapter.getInitialState(),
+  modifierOptions: ModifierOptionsAdapter.getInitialState(),
+  products: IProductEntriesAdapter.getInitialState(),
   productInstances: IProductInstancesAdapter.getInitialState(),
-  categories: ICategoriesAdapter.getInitialState(),
+  categories: ICategoryEntriesAdapter.getInitialState(),
   productInstanceFunctions: ProductInstanceFunctionsAdapter.getInitialState(),
   orderInstanceFunctions: OrderInstanceFunctionsAdapter.getInitialState(),
   settings: null,
@@ -91,13 +103,13 @@ const SocketIoSlice = createSlice({
     },
     receiveCatalog(state, action: PayloadAction<ICatalog>) {
       state.catalog = action.payload;
-      IOptionTypesAdapter.setAll(state.modifiers, Object.values(action.payload.modifiers).map(x => x.modifierType));
-      IOptionsAdapter.setAll(state.modifierOptions, Object.values(action.payload.options));
-      IProductsAdapter.setAll(state.products, Object.values(action.payload.products).map(x => x.product));
-      IProductInstancesAdapter.setAll(state.productInstances, Object.values(action.payload.productInstances));
-      ICategoriesAdapter.setAll(state.categories, Object.values(action.payload.categories).map(x => x.category));
-      ProductInstanceFunctionsAdapter.setAll(state.productInstanceFunctions, Object.values(action.payload.productInstanceFunctions));
-      OrderInstanceFunctionsAdapter.setAll(state.orderInstanceFunctions, Object.values(action.payload.orderInstanceFunctions));
+      ModifierTypeEntriesAdapter.setAll(state.modifierEntries, action.payload.modifiers);
+      ModifierOptionsAdapter.setAll(state.modifierOptions, action.payload.options);
+      IProductEntriesAdapter.setAll(state.products, action.payload.products);
+      IProductInstancesAdapter.setAll(state.productInstances, action.payload.productInstances);
+      ICategoryEntriesAdapter.setAll(state.categories, action.payload.categories);
+      ProductInstanceFunctionsAdapter.setAll(state.productInstanceFunctions, action.payload.productInstanceFunctions);
+      OrderInstanceFunctionsAdapter.setAll(state.orderInstanceFunctions, action.payload.orderInstanceFunctions);
     },
     receiveFulfillments(state, action: PayloadAction<Record<string, FulfillmentConfig>>) {
       state.fulfillments = action.payload;
@@ -119,8 +131,23 @@ const SocketIoSlice = createSlice({
   }
 });
 
-export const { selectAll: getProductInstanceFunctions, selectById: getProductInstanceFunctionById } =
-  ProductInstanceFunctionsAdapter.getSelectors();
+export const CatalogSelectors = (state: SocketIoState): ICatalogSelectors => ({
+  categories: () => getCategoryEntryIds(state.categories) as string[],
+  category: (id) => getCategoryEntryById(state.categories, id),
+  modifierEntries: () => getModifierTypeEntryIds(state.modifierEntries) as string[],
+  modifierEntry: (id) => getModifierTypeEntryById(state.modifierEntries, id),
+  options: () => getModifierOptionIds(state.modifierOptions) as string[],
+  option: (id) => getModifierOptionById(state.modifierOptions, id),
+  productEntries: () => getProductEntryIds(state.products) as string[],
+  productEntry: (id) => getProductEntryById(state.products, id),
+  productInstances: () => getProductInstanceIds(state.productInstances) as string[],
+  productInstance: (id) => getProductInstanceById(state.productInstances, id),
+  orderInstanceFunctions: () => getOrderInstanceFunctionIds(state.orderInstanceFunctions) as string[],
+  orderInstanceFunction: (id) => getOrderInstanceFunctionById(state.orderInstanceFunctions, id),
+  productInstanceFunctions: () => getProductInstanceFunctionIds(state.productInstanceFunctions) as string[],
+  productInstanceFunction: (id) => getProductInstanceFunctionById(state.productInstanceFunctions, id)
+})
+
 export const SocketIoActions = SocketIoSlice.actions;
 export const SocketIoReducer = SocketIoSlice.reducer;
 export const IsSocketDataLoaded = (s: SocketIoState) => s.serverTime !== null && s.fulfillments !== null && s.catalog !== null && s.settings !== null;
