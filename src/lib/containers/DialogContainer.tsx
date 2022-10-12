0f848ba67f416f849afcc25226b0ae5739b53693
Dave Lichterman <me@lavid.me>
2022-10-11 18:59:56 -0700

    v 0.1.12
    * package updates
    * optional title on DialogContainer

--- src/lib/containers/DialogContainer.tsx
+++ src/lib/containers/DialogContainer.tsx
@@ -2,7 +2,7 @@ import { Divider, DialogTitle, Dialog, DialogProps } from '@mui/material';
 
 export interface IDialogContainer {
   onClose: Exclude<DialogProps['onClose'], undefined>;
-  title: string;
+  title?: React.ReactNode;
   innerComponent: React.ReactNode;
   open: boolean;
 };
@@ -10,8 +10,8 @@ export interface IDialogContainer {
 export const DialogContainer = function ({ onClose, title, open, innerComponent: inner_component, ...other }: IDialogContainer & Omit<DialogProps, 'onClose' | 'open'>) {
   return (
     <Dialog {...other} open={open} onClose={onClose}>
-      <DialogTitle>{title}</DialogTitle>
-      <Divider />
+      {title && <><DialogTitle>{title}</DialogTitle>
+        <Divider /></>}
       {inner_component}
     </Dialog>
   );
