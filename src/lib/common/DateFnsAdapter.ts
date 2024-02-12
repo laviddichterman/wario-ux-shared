46a7e2b251ee4605ef595a9a39d78fafb9d9bc36
Dave Lichterman <me@lavid.me>
2024-02-12 11:58:52 -0800

    v 1.1.2 fuck around find out
    * WIP attempt to upgrade to date-fns 3.x
    * migrate DateFnsAdapter
    * move this pkg deps into peer deps

--- src/lib/common/DateFnsAdapter.ts
+++ src/lib/common/DateFnsAdapter.ts
@@ -1,13 +1,43 @@
-import { default as AdapterDateFns } from '@date-io/date-fns';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
 import { toDate as toDateBase } from 'date-fns';
+
+type DateBuilderReturnType<T extends string | null | undefined, TDate> = T extends null
+  ? null
+  : TDate;
 
 export const AdapterCurrentTimeOverrideUtils = (now: Date | number) =>
   class AdapterDateFnsWrapper extends AdapterDateFns {
-    public date = (value?: any) => {
-      if (typeof value === "undefined" || value === null) {
-        return toDateBase(now);
+    public date = <T extends string | null | undefined>(
+      value?: T,
+    ): DateBuilderReturnType<T, Date> => {
+      type R = DateBuilderReturnType<T, Date>;
+      if (typeof value === 'undefined') {
+        return <R>toDateBase(now);
       }
-      return new Date(value);
+
+      if (value === null) {
+        return <R>null;
+      }
+
+      return <R>new Date(value);
     };
 
-  }
+    // date<
+    //   TArg extends unknown = undefined,
+    //   TRes extends unknown = TArg extends null
+    //   ? null
+    //   : TArg extends undefined
+    //   ? Date
+    //   : Date | null
+    // >(value?: TArg): TRes {
+    //   if (typeof value === "undefined") {
+    //     return toDateBase(now) as TRes;
+    //   }
+
+    //   if (value === null) {
+    //     return null as TRes;
+    //   }
+
+    //   return toDateBase(value as string | number | Date) as TRes;
+    // }
+  };
