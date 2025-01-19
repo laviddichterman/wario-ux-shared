6038cd7da399ede36fedbc76d0c301e6d9ee8bec
Dave Lichterman <me@lavid.me>
2025-01-19 14:18:12 -0800

    v 2.0.12 well, bring back the adapter i guess

--- src/lib/common/DateFnsAdapter.ts
+++ src/lib/common/DateFnsAdapter.ts
@@ -1,21 +1,21 @@
-// import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
-// import { DateBuilderReturnType } from '@mui/x-date-pickers/models';
-// import { toDate as toDateBase } from 'date-fns';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsV3';
+import { DateBuilderReturnType } from '@mui/x-date-pickers/models';
+import { toDate as toDateBase } from 'date-fns';
 
-// export const AdapterCurrentTimeOverrideUtils = (now: Date | number) =>
-//   class AdapterDateFnsWrapper extends AdapterDateFns {
-//     public date = <T extends string | null | undefined>(
-//       value?: T,
-//     ): DateBuilderReturnType<T, Date> => {
-//       type R = DateBuilderReturnType<T, Date>;
-//       if (typeof value === 'undefined') {
-//         return <R>toDateBase(now);
-//       }
+export const AdapterCurrentTimeOverrideUtils = (now: Date | number) =>
+  class AdapterDateFnsWrapper extends AdapterDateFns {
+    public date = <T extends string | null | undefined>(
+      value?: T,
+    ): DateBuilderReturnType<T, Date> => {
+      type R = DateBuilderReturnType<T, Date>;
+      if (typeof value === 'undefined') {
+        return <R>toDateBase(now);
+      }
 
-//       if (value === null) {
-//         return <R>null;
-//       }
+      if (value === null) {
+        return <R>null;
+      }
 
-//       return <R>new Date(value);
-//     };
-//   };
+      return <R>new Date(value);
+    };
+  };
